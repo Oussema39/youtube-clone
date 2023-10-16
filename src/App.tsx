baac2ddc4d0b87b17b48b8b320be422b387a2ee5
Oussema Heni <oussemaheny2@gmail.com>
2023-10-16 19:46:04 +0100

    added categories ships

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import CategoryShips from "./components/CategoryShips";
+import { Categories } from "./data/placeholders";
 import Header from "./layouts/Header";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("all");
   return (
     <div className="max-h-screen flex flex-col">
       <Header />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryShips
+              categories={Categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
